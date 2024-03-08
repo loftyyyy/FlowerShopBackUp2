929f9926d525eb95de89902e2d8615762535a7d2
feliciety <feannemlsrte@gmail.com>
2024-03-09 04:06:38 +0800

    homepage new arrival

--- src/main/java/com/example/fs/HomePageController.java
+++ src/main/java/com/example/fs/HomePageController.java
@@ -6,12 +6,14 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 import java.util.Objects;
 
 public class HomePageController {
+
 
     @FXML
     private Pane pane1;
@@ -58,6 +60,12 @@ public class HomePageController {
     @FXML
     private Label meaning4;
 
+    private MarketController marketController;
+
+    public void setMarketController(MarketController marketController) {
+        this.marketController = marketController;
+    }
+
     @FXML
     public void pane1MouseEntered(MouseEvent mouseEvent) {
         handlePaneMouseEntered(mouseEvent, pane1);
@@ -98,10 +106,6 @@ public class HomePageController {
         handlePaneMouseExited(mouseEvent, pane4);
     }
 
-    MarketController marketController;
-    public void setMarketController(MarketController marketController){
-        this.marketController = marketController;
-    }
 
     private void handlePaneMouseEntered(MouseEvent event, Pane pane) {
         ImageView image = getImageForPane(pane);
@@ -165,6 +169,144 @@ public class HomePageController {
         ParallelTransition parallelTransition = new ParallelTransition(meaningTranslate, paneScale);
         parallelTransition.play();
     }
+    @FXML
+    private ImageView flower1;
+    @FXML
+    private ImageView flower2;
+    @FXML
+    private ImageView flower3;
+    @FXML
+    private ImageView flower4;
+
+    @FXML
+    private Pane flowerCard1;
+    @FXML
+    private Pane flowerCard2;
+    @FXML
+    private Pane flowerCard3;
+    @FXML
+    private Pane flowerCard4;
+
+    @FXML
+    public void flowerEnter1(MouseEvent event) {
+        handleImageViewZoomIn(flower1);
+    }
+
+    @FXML
+    public void flowerExit1(MouseEvent event) {
+        handleImageViewZoomOut(flower1);
+    }
+
+    @FXML
+    public void flowerEnter2(MouseEvent event) {
+        handleImageViewZoomIn(flower2);
+    }
+
+    @FXML
+    public void flowerExit2(MouseEvent event) {
+
+        handleImageViewZoomOut(flower2);
+    }
+
+    @FXML
+    public void flowerEnter3(MouseEvent event) {
+
+        handleImageViewZoomIn(flower3);
+    }
+
+    @FXML
+    public void flowerExit3(MouseEvent event) {
+
+        handleImageViewZoomOut(flower3);
+    }
+
+    @FXML
+    public void flowerEnter4(MouseEvent event) {
+
+        handleImageViewZoomIn(flower4);
+    }
+
+    @FXML
+    public void flowerExit4(MouseEvent event) {
+
+        handleImageViewZoomOut(flower4);
+    }
+
+    @FXML
+    public void flowerCardEnter1(MouseEvent event) {
+        handlePaneZoomIn(flowerCard1);
+    }
+
+    @FXML
+    public void flowerCardExit1(MouseEvent event) {
+        handlePaneZoomOut(flowerCard1);
+    }
+    @FXML
+    public void flowerCardEnter2(MouseEvent event) {
+
+        handlePaneZoomIn(flowerCard2);
+    }
+
+    @FXML
+    public void flowerCardExit2(MouseEvent event) {
+
+        handlePaneZoomOut(flowerCard2);
+    }
+
+    @FXML
+    public void flowerCardEnter3(MouseEvent event) {
+
+        handlePaneZoomIn(flowerCard3);
+    }
+
+    @FXML
+    public void flowerCardExit3(MouseEvent event) {
+
+        handlePaneZoomOut(flowerCard3);
+    }
+
+    @FXML
+    public void flowerCardEnter4(MouseEvent event) {
+
+        handlePaneZoomIn(flowerCard4);
+    }
+
+    @FXML
+    public void flowerCardExit4(MouseEvent event) {
+
+        handlePaneZoomOut(flowerCard4);
+    }
+
+    private void handleImageViewZoomIn(ImageView imageView) {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.5), imageView);
+        double zoomFactor = 1.2;
+        scaleTransition.setToX(zoomFactor);
+        scaleTransition.setToY(zoomFactor);
+        scaleTransition.play();
+    }
+
+    private void handleImageViewZoomOut(ImageView imageView) {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.5), imageView);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.play();
+    }
+
+    private void handlePaneZoomIn(Pane pane) {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.5), pane);
+        double zoomFactor = 1.2;
+        scaleTransition.setToX(zoomFactor);
+        scaleTransition.setToY(zoomFactor);
+        scaleTransition.play();
+    }
+
+    private void handlePaneZoomOut(Pane pane) {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.5), pane);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.play();
+    }
+
 
     @FXML
     private ImageView heartIcon1;
@@ -184,6 +326,22 @@ public class HomePageController {
     private boolean isRed2 = false;
     private boolean isRed3 = false;
     private boolean isRed4 = false;
+
+    @FXML
+    private ImageView cartIcon1;
+    @FXML
+    private ImageView cartIcon2;
+    @FXML
+    private ImageView cartIcon3;
+    @FXML
+    private ImageView cartIcon4;
+
+    private Image grayCartImage;
+    private Image blueCartImage;
+    private boolean isBlue1 = false;
+    private boolean isBlue2 = false;
+    private boolean isBlue3 = false;
+    private boolean isBlue4 = false;
 
     @FXML
     void clickHeart1(MouseEvent event) {
@@ -245,6 +403,66 @@ public class HomePageController {
         }
     }
 
+    @FXML
+    void clickCart1(MouseEvent event) {
+        toggleCartIcon(cartIcon1);
+        playPumpAnimation(cartIcon1);
+    }
+
+    @FXML
+    void clickCart2(MouseEvent event) {
+        toggleCartIcon(cartIcon2);
+        playPumpAnimation(cartIcon2);
+    }
+
+    @FXML
+    void clickCart3(MouseEvent event) {
+        toggleCartIcon(cartIcon3);
+        playPumpAnimation(cartIcon3);
+    }
+
+    @FXML
+    void clickCart4(MouseEvent event) {
+        toggleCartIcon(cartIcon4);
+        playPumpAnimation(cartIcon4);
+    }
+
+    private void toggleCartIcon(ImageView cartIcon) {
+        if (cartIcon.equals(cartIcon1)) {
+            if (isBlue1) {
+                cartIcon.setImage(grayCartImage);
+                isBlue1 = false;
+            } else {
+                cartIcon.setImage(blueCartImage);
+                isBlue1 = true;
+            }
+        } else if (cartIcon.equals(cartIcon2)) {
+            if (isBlue2) {
+                cartIcon.setImage(grayCartImage);
+                isBlue2 = false;
+            } else {
+                cartIcon.setImage(blueCartImage);
+                isBlue2 = true;
+            }
+        } else if (cartIcon.equals(cartIcon3)) {
+            if (isBlue3) {
+                cartIcon.setImage(grayCartImage);
+                isBlue3 = false;
+            } else {
+                cartIcon.setImage(blueCartImage);
+                isBlue3 = true;
+            }
+        } else if (cartIcon.equals(cartIcon4)) {
+            if (isBlue4) {
+                cartIcon.setImage(grayCartImage);
+                isBlue4 = false;
+            } else {
+                cartIcon.setImage(blueCartImage);
+                isBlue4 = true;
+            }
+        }
+    }
+
     private void playPumpAnimation(ImageView heartIcon) {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(heartIcon.scaleXProperty(), 1), new KeyValue(heartIcon.scaleYProperty(), 1)),
@@ -256,14 +474,20 @@ public class HomePageController {
 
     @FXML
     public void initialize() {
-        // Load images
         grayHeartImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/grayheart.png")));
         redHeartImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/redheart.png")));
+        grayCartImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/cartgrey.png")));
+        blueCartImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/cartblue.png")));
 
-        // Set initial image
         heartIcon1.setImage(grayHeartImage);
         heartIcon2.setImage(grayHeartImage);
         heartIcon3.setImage(grayHeartImage);
         heartIcon4.setImage(grayHeartImage);
+        cartIcon1.setImage(grayCartImage);
+        cartIcon2.setImage(grayCartImage);
+        cartIcon3.setImage(grayCartImage);
+        cartIcon4.setImage(grayCartImage);
+
     }
+
 }
